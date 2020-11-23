9713838d93bfaa84aa216a7867d7fe1d7ab8cb31
Eduard Batiushchenko <72995778+Eduard-Batiushchenko@users.noreply.github.com>
2020-11-23 16:16:31 +0200

    Сделал на все анимацию, нужно потестить

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,33 +1,5 @@
 $(document).ready(function () {
-    
-//     $('#reviews-slider').slick({
-//         //   rtl: true
-//         asNavFor: '#gallery'
-//     });
-//     $('#gallery').slick({
-//          responsive: [
-//     {
-//       breakpoint: 768,
-//       settings: {
-//         arrows: false,
-//         centerMode: false,
-//         infinite: true,
-//           slidesToShow: 3,
-//             slidesToScroll: 3
-//       }
-//              },
-//                 {
-//       breakpoint: 2000,
-//       settings: {
-//         arrows: false,
-//           centerMode: false,
-//         infinite: false,
-//           slidesToShow: 7,
-//             slidesToScroll: 3,
-//       }
-//              }
-//   ]
-//     });
+
     
     
   $('#reviews-slider').slick({
@@ -82,8 +54,13 @@ $(document).ready(function () {
   // dots: false,
   // centerMode: false,
   // focusOnSelect: true
-});
+  });
+  
+
+  new WOW().init({
     
+  });
+
     
 });
 
